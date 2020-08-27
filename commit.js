bdd9c34c232c8f9471a7987284b858de7402312c
Andrés Felipe Naranjo Quintero <afnarqui@hotmail.com>
2020-08-26 20:26:05 -0500

    guardando los commits anteriores

---
+++ commit.js
@@ -0,0 +1,11 @@
+commit 9ced330014feebc927616f225b09f70b996f3201 (HEAD -> master)
+Author: Andrés Felipe Naranjo Quintero <afnarqui@hotmail.com>   
+Date:   Wed Aug 26 20:23:34 2020 -0500
+
+    se ingresa h1 para visualizar mi primera página web
+
+commit 687dd9185ed4aa1b2b00ba4da2b3d88e0c461c50
+Author: Andrés Felipe Naranjo Quintero <afnarqui@hotmail.com>   
+Date:   Wed Aug 26 20:16:24 2020 -0500
+
+    mi primir archivo guardado
