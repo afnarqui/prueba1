23042f772c83aad916c1b402f81fd4500e8eca4e
Andrés Felipe Naranjo Quintero <afnarqui@hotmail.com>
2020-08-26 20:51:19 -0500

    buscando generar conflictos

--- commit.js
+++ commit.js
@@ -1,5 +1,5 @@
 commit 9ced330014feebc927616f225b09f70b996f3201 (HEAD -> master)
-Author: Andrés Felipe Naranjo Quintero <afnarqui@hotmail.com>   
+Author: Andrés Felipe Naranjo Quintero git lo mejor <afnarqui@hotmail.com>   
 Date:   Wed Aug 26 20:23:34 2020 -0500
 
     se ingresa h1 para visualizar mi primera página web
